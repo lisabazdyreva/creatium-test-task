5b8d28aa0b53fbf21128334126cfd4505d77b96b
lisabazdyreva <lisabazdryreva@gmail.com>
2024-12-02 18:16:40 +0300

    init project

---
+++ test-task/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+import LayoutHeader from '@/components/layout/LayoutHeader.vue';
+</script>
+
+<template>
+  <LayoutHeader />
+  <RouterView />
+</template>
+
+<style scoped></style>

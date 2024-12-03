84422605a4a47b363dd1ff1dee46e54cf23f60d4
lisabazdyreva <lisabazdryreva@gmail.com>
2024-12-03 16:10:23 +0300

    added and format icons

--- test-task/src/components/icons/actions/CodeIcon.vue
+++ test-task/src/components/icons/actions/CodeIcon.vue
@@ -1,7 +1,27 @@
 <template>
-  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-    <path d="M6.66667 5.18519L2.22223 10.1734L6.66667 14.8148" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
-    <path d="M13.3333 5.18519L17.7778 10.1734L13.3333 14.8148" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
-    <path d="M11.1111 1.48148L8.88892 17.7778" stroke="currentColor" stroke-linecap="round"/>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+  >
+    <path
+      d="M6.66667 5.18519L2.22223 10.1734L6.66667 14.8148"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M13.3333 5.18519L17.7778 10.1734L13.3333 14.8148"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M11.1111 1.48148L8.88892 17.7778"
+      stroke="currentColor"
+      stroke-linecap="round"
+    />
   </svg>
 </template>
--- test-task/src/components/icons/actions/ResponseIcon.vue
+++ test-task/src/components/icons/actions/ResponseIcon.vue
@@ -1,14 +1,39 @@
 <template>
-  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+  >
     <g clip-path="url(#clip0_300_282)">
-      <path d="M20 0H0V20H20V0Z" fill="white" fill-opacity="0.01"/>
-      <path fill-rule="evenodd" clip-rule="evenodd" d="M10 18.3333C14.6024 18.3333 18.3334 14.6024 18.3334 10C18.3334 5.39763 14.6024 1.66668 10 1.66668C5.39765 1.66668 1.66669 5.39763 1.66669 10C1.66669 14.6024 5.39765 18.3333 10 18.3333Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
-      <path d="M2 9.5L18 9.5" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
-      <path fill-rule="evenodd" clip-rule="evenodd" d="M10 18.3333C11.841 18.3333 13.3334 14.6024 13.3334 10C13.3334 5.39763 11.841 1.66668 10 1.66668C8.15906 1.66668 6.66669 5.39763 6.66669 10C6.66669 14.6024 8.15906 18.3333 10 18.3333Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M20 0H0V20H20V0Z" fill="white" fill-opacity="0.01" />
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M10 18.3333C14.6024 18.3333 18.3334 14.6024 18.3334 10C18.3334 5.39763 14.6024 1.66668 10 1.66668C5.39765 1.66668 1.66669 5.39763 1.66669 10C1.66669 14.6024 5.39765 18.3333 10 18.3333Z"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M2 9.5L18 9.5"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M10 18.3333C11.841 18.3333 13.3334 14.6024 13.3334 10C13.3334 5.39763 11.841 1.66668 10 1.66668C8.15906 1.66668 6.66669 5.39763 6.66669 10C6.66669 14.6024 8.15906 18.3333 10 18.3333Z"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
     </g>
     <defs>
       <clipPath id="clip0_300_282">
-        <rect width="20" height="20" fill="white"/>
+        <rect width="20" height="20" fill="white" />
       </clipPath>
     </defs>
   </svg>
